609a4e8c701c820772a96a22d5480a3b2ae4e746
Vanessa <vanessawilliams2598@gmail.com>
2026-04-10 18:58:30 -0400

    modify
    modified silly story generator, fixed syntax errors, added pictures to gallery challenge

--- Image Gallery challenge/main.js
+++ Image Gallery challenge/main.js
@@ -3,3 +3,67 @@ const thumbBar = document.querySelector(".thumb-bar");
 
 const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
+
+// Solution: Create a data object
+
+const images = [
+  { filename: "pic1.jpg", alt: "Closeup of a human eye" },
+  { filename: "pic2.jpg", alt: "Rock that looks like a wave" },
+  { filename: "pic3.jpg", alt: "Purple and white pansies" },
+  { filename: "pic4.jpg", alt: "Section of wall from a pharaoh's tomb" },
+  { filename: "pic5.jpg", alt: "Large moth on a leaf" },
+];
+
+// Solution: Loop through the images
+
+// Create a baseURL constant containing the baseURL of the images
+const baseURL =
+  "https://mdn.github.io/shared-assets/images/examples/learn/gallery/";
+
+// Loop through the images using a for...of loop
+for (const image of images) {
+  // Create a new image element
+  const newImage = document.createElement("img");
+  // Set the source and alt text for the image
+  newImage.src = `${baseURL}${image.filename}`;
+  newImage.alt = image.alt;
+  // Make the image focusable via the keyboard
+  newImage.tabIndex = "0";
+  // Append the image as a child of the thumbBar
+  thumbBar.appendChild(newImage);
+  // Update the display to show the image full size when a thumb is clicked
+  newImage.addEventListener("click", updateDisplayedImage);
+  // Update the display to show the image full size when the "Enter" key
+  // is pressed after it has been focused
+  newImage.addEventListener("keydown", (e) => {
+    if (e.code === "Enter") {
+      updateDisplayedImage(e);
+    }
+  });
+}
+
+// Solution: Create the updateDisplayedImage() function
+
+function updateDisplayedImage(e) {
+  displayedImage.src = e.target.src;
+  displayedImage.alt = e.target.alt;
+}
+
+// Solution: Wire up the Darken/Lighten button
+
+// Add a click event listener on the button
+btn.addEventListener("click", () => {
+  // If the button has a "dark" class set,
+  // change text to "Lighten" and make the overlay darker
+  if (btn.classList.contains("dark")) {
+    btn.textContent = "Lighten";
+    overlay.style.backgroundColor = "rgb(0 0 0 / 0.5)";
+  } else {
+    // Else, change text to "Darken" and make
+    // the overlay lighter
+    btn.textContent = "Darken";
+    overlay.style.backgroundColor = "rgb(0 0 0 / 0)";
+  }
+  // Toggle the class ready for the next button press
+  btn.classList.toggle("dark");
+});
--- Silly_story_generator/main.js
+++ Silly_story_generator/main.js
@@ -1,44 +1,55 @@
-const customName = document.getElementById('customname');
-const randomize = document.querySelector('.randomize');
-const story = document.querySelector('.story');
+// references to the input field, button and story paragraph
 
+const customName = document.getElementById("customname");
+const generateBtn = document.querySelector(".randomize");
+const story = document.querySelector(".story");
+
+// returns random item from array
 function randomValueFromArray(array) {
   const random = Math.floor(Math.random() * array.length);
   return array[random];
 }
 
-let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
-let insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
-let insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
-let insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
+// arrays containing possible characters/places/events
+const characters = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+const places = ["the soup kitchen", "Disneyland", "the White House"];
+const events = [
+  "spontaneously combusted",
+  "melted into a puddle on the sidewalk",
+  "turned into a slug and slithered away",
+];
 
-randomize.addEventListener('click', result);
 
-function result() {
-  let newStory = storyText;
+// make complete random story string(s)
+function returnRandomStoryString() {
+  const randomCharacter = randomValueFromArray(characters);
+  const randomPlace = randomValueFromArray(places);
+  const randomEvent = randomValueFromArray(events);
+  // base story template  
+  let storyText = `It was 94 Fahrenheit outside, so ${randomCharacter} went for a walk. When they got to ${randomPlace}, they stared in horror for a few moments, then ${randomEvent}. Bob saw the whole thing, but was not surprised — ${randomCharacter} weighs 300 pounds, and it was a hot day.`;
 
-  const xItem = randomValueFromArray(insertX);
-  const yItem = randomValueFromArray(insertY);
-  const zItem = randomValueFromArray(insertZ);
-
-  newStory = newStory.replaceAll(':insertx:', xItem);
-  newStory = newStory.replace(':inserty:', yItem);
-  newStory = newStory.replace(':insertz:', zItem);
-
-  if (customName.value !== '') {
-    const name = customName.value;
-    newStory = newStory.replace('Bob', name);
-  }
-
-  const uk = document.getElementById('uk');
-  if (uk.checked) {
-    const weight = Math.round(300 * 0.0714286) + ' stone';
-    const temperature = Math.round((94 - 32) * 5 / 9) + ' centigrade';
-
-    newStory = newStory.replace('94 fahrenheit', temperature);
-    newStory = newStory.replace('300 pounds', weight);
-  }
-
-  story.textContent = newStory;
-  story.style.visibility = 'visible';
+  return storyText;
 }
+
+// generates new story when clicked
+generateBtn.addEventListener("click", generateStory);
+
+function generateStory() {
+  let newStory = returnRandomStoryString();
+  // replace with custom name
+  if (customName.value !== "") {
+    const name = customName.value;
+    newStory = newStory.replace("Bob", name);
+  }
+  // if uk button pressed, it will convert units
+  if (document.getElementById("uk").checked) {
+    const weight = `${Math.round(300 / 14)} stone`;
+    const temperature = `${Math.round((94 - 32) * (5 / 9))} Celsius`;
+    newStory = newStory.replace("300 pounds", weight);
+    newStory = newStory.replace("94 Fahrenheit", temperature);
+  }
+  // displays story on page
+  story.textContent = newStory;
+  story.style.visibility = "visible";
+}
+
